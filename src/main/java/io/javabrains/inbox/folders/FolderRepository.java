140706e7dd3206ed6d8b6543cff1b48f1ff1761d
DorotaPotulska <cdc4@wp.pl>
2022-02-15 23:26:44 +0100

    Add Email view and fix navigation

--- src/main/java/io/javabrains/inbox/folders/FolderRepository.java
+++ src/main/java/io/javabrains/inbox/folders/FolderRepository.java
@@ -3,7 +3,6 @@ package io.javabrains.inbox.folders;
 import java.util.List;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FolderRepository extends CassandraRepository<Folder, String>{
